d785004986ba4429000d18c2692b1572544e9c9b
hack <hacktavist616@gmail.com>
2016-08-16 19:57:06 -0400

    gonna try building android before removing unity ads

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,16 +1,16 @@
 #! /bin/sh
 
 # Change this the name of your project. This will be the name of the final executables as well.
-project="TestingTravis"
+project="FZR"
 
-echo "Attempting to build $project for Windows"
+echo "Attempting to build $project for Android"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+  -buildTarget android # "$(pwd)/Build/windows/$project.exe" \
   -quit
 
 
