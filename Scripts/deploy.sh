c7a34d112ad5085c95540c393b8e39b36aec0754
hack <hacktavist616@gmail.com>
2016-08-16 21:18:42 -0400

    adding a shell script for deploying the windows build to itch

---
+++ Scripts/deploy.sh
@@ -0,0 +1,16 @@
+FILENAME="$(pwd)"
+
+zip -r "$FILENAME.zip" $FILENAME
+
+echo "Deploy to Itch"
+wget http://dl.itch.ovh/butler/darwin-amd64/head/butler
+chmod +x butler
+touch butler_creds
+echo -n $ITCH_API_KEY > butler_creds
+
+./butler push $(pwd).zip hacktavist/testing-travisci-deployment:windows -i butler_creds
+
+echo "Cleaning"
+
+./butler logout -i butler_creds --assume-yes
+rm butler
