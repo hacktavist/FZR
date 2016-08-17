403aafee5e1c2659a27302159e1f451c1b8230ec
hack <hacktavist616@gmail.com>
2016-08-16 20:59:41 -0400

    reverting build file to build windows player

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -3,14 +3,14 @@
 # Change this the name of your project. This will be the name of the final executables as well.
 project="FZR"
 
-echo "Attempting to build $project for Android"
+echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
-  -buildTarget android # "$(pwd)/Build/windows/$project.exe" \
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
   -quit
 
 
